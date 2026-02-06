6bed9d8e20df47034270082a5a07f5e6c3972703
kunzroyal <dung.vt210602@gmail.com>
2026-02-06 11:20:18 +0700

    update

--- DOANTOTNGHIEP/api/db.js
+++
@@ -1,8 +0,0 @@
-import mysql from "mysql2";
-export const db = mysql.createConnection({
-  host: "localhost",
-  port: "3306",
-  user: "root",
-  password: "Tao21062002",
-  database: "tutoringcenterdb2",
-});
